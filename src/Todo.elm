9e65c3e3645df27e76d17004dbfee0c165c6adb5
Janis Miezitis <janjiss@gmail.com>
2019-05-09 21:41:14 +0300

    Hello World example

--- src/Todo.elm
+++ src/Todo.elm
@@ -1,4 +1,4 @@
-port module Todo exposing (Entry, Model, Msg(..), emptyModel, init, main, onEnter, setStorage, showEntries, showEntry, showFooter, showHeader, update, updateWithStorage, view)
+module Todo exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Html exposing (..)
@@ -14,205 +14,29 @@ main =
     Browser.element
         { init = init
         , view = view
-        , update = updateWithStorage
+        , update = update
         , subscriptions = \_ -> Sub.none
         }
 
 
-emptyModel =
-    { entries = []
-    , currentUid = 0
-    , currentInput = ""
-    , filter = "All"
-    }
+type Msg
+    = All
 
 
 init : Maybe Model -> ( Model, Cmd msg )
-init storedModel =
-    ( Maybe.withDefault emptyModel storedModel, Cmd.none )
-
-
-port setStorage : Model -> Cmd msg
+init _ =
+    ( "", Cmd.none )
 
 
 type alias Model =
-    { entries : List Entry
-    , currentUid : Int
-    , currentInput : String
-    , filter : String
-    }
-
-
-type alias Entry =
-    { uid : Int
-    , description : String
-    , isCompleted : Bool
-    }
-
-
-type Filter
-    = Completed
-    | All
-    | Active
-
-
-type Msg
-    = Add
-    | ChangeInput String
-    | Check Int
-    | Delete Int
-    | CheckAll
-    | ChangeFilter String
-    | ClearCompleted
-
-
-updateWithStorage : Msg -> Model -> ( Model, Cmd Msg )
-updateWithStorage msg model =
-    let
-        ( newModel, command ) =
-            update msg model
-    in
-    ( newModel
-    , Cmd.batch [ setStorage newModel, command ]
-    )
+    String
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
-    case msg of
-        Add ->
-            let
-                newEntriesList =
-                    List.append model.entries [ Entry model.currentUid model.currentInput False ]
-            in
-            ( { model | entries = newEntriesList, currentInput = "", currentUid = model.currentUid + 1 }, Cmd.none )
-
-        ChangeInput newInput ->
-            ( { model | currentInput = newInput }, Cmd.none )
-
-        Check uid ->
-            let
-                checkEntry entry =
-                    if entry.uid == uid then
-                        { entry | isCompleted = not entry.isCompleted }
-
-                    else
-                        entry
-            in
-            ( { model | entries = List.map checkEntry model.entries }, Cmd.none )
-
-        CheckAll ->
-            let
-                allCompleted =
-                    List.all .isCompleted model.entries
-
-                updateEntry e =
-                    { e | isCompleted = not allCompleted }
-            in
-            ( { model | entries = List.map updateEntry model.entries }, Cmd.none )
-
-        Delete uid ->
-            ( { model | entries = List.filter (\e -> not (e.uid == uid)) model.entries }, Cmd.none )
-
-        ChangeFilter filter ->
-            ( { model | filter = filter }, Cmd.none )
-
-        ClearCompleted ->
-            ( { model | entries = List.filter (\e -> not e.isCompleted) model.entries }, Cmd.none )
+    ( model, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "todomvc-wrapper" ]
-        [ section [ class "todoapp" ]
-            [ showHeader model
-            , showEntries model
-            , showFooter model
-            ]
-        ]
-
-
-showHeader : Model -> Html Msg
-showHeader model =
-    header [ class "header" ]
-        [ h1 [] [ text "todos" ]
-        , input [ class "new-todo", value model.currentInput, placeholder "What needs to be done?", onEnter Add, onInput ChangeInput ] []
-        ]
-
-
-showEntries : Model -> Html Msg
-showEntries model =
-    let
-        entries =
-            case model.filter of
-                "Completed" ->
-                    List.filter (\e -> e.isCompleted) model.entries
-
-                "Active" ->
-                    List.filter (\e -> not e.isCompleted) model.entries
-
-                _ ->
-                    model.entries
-    in
-    section [ class "main" ]
-        [ input [ class "toggle-all", type_ "checkbox", onClick CheckAll ] []
-        , ul [ class "todo-list" ] (List.map showEntry entries)
-        ]
-
-
-showEntry : Entry -> Html Msg
-showEntry entry =
-    li [ classList [ ( "completed", entry.isCompleted ) ] ]
-        [ div [ class "view" ]
-            [ input [ class "toggle", type_ "checkbox", onClick (Check entry.uid), checked entry.isCompleted ] []
-            , label [] [ text entry.description ]
-            , button [ class "destroy", onClick (Delete entry.uid) ] []
-            ]
-        ]
-
-
-showFooter model =
-    let
-        itemsLeftInTodo =
-            List.filter (\e -> not e.isCompleted) model.entries
-                |> List.length
-                |> String.fromInt
-
-        itemsDone =
-            List.filter (\e -> e.isCompleted) model.entries
-                |> List.length
-                |> String.fromInt
-    in
-    footer [ class "footer" ]
-        [ span [ class "todo-count" ]
-            [ text itemsLeftInTodo
-            , text " items left"
-            ]
-        , ul [ class "filters" ]
-            [ li []
-                [ a [ classList [ ( "selected", model.filter == "All" ) ], onClick (ChangeFilter "All") ] [ text "All" ]
-                ]
-            , li []
-                [ a [ classList [ ( "selected", model.filter == "Active" ) ], onClick (ChangeFilter "Active") ] [ text "Active" ]
-                ]
-            , li []
-                [ a [ classList [ ( "selected", model.filter == "Completed" ) ], onClick (ChangeFilter "Completed") ] [ text "Completed" ]
-                ]
-            ]
-        , button [ class "clear-completed", onClick ClearCompleted ]
-            [ text ("Clear Completed (" ++ itemsDone ++ ")")
-            ]
-        ]
-
-
-onEnter : Msg -> Attribute Msg
-onEnter msg =
-    let
-        isEnter key =
-            if key == 13 then
-                Json.succeed msg
-
-            else
-                Json.fail "Not Enter"
-    in
-    on "keydown" (Json.andThen isEnter keyCode)
+    text "Hello World"
--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ require("./styles.scss");
 
 const { Elm } = require('./Todo');
 
-const storedState = localStorage.getItem('elm-todo-save');
-const startingState = storedState ? JSON.parse(storedState) : null;
-
 const todomvc = Elm.Todo.init({
     node: document.getElementById('elm'),
-    flags: startingState,
-});
-
-todomvc.ports.setStorage.subscribe(function(state) {
-    localStorage.setItem('elm-todo-save', JSON.stringify(state));
+    flags: null
 });
